07aaef5980a36b025763bc3a24810c9991e646e8
Yurii_Popovskiy <ronondex663@gmail.com>
2025-05-17 17:41:49 +0300

    AddTestToReverse

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,7 +8,8 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse([])).toBeInstanceOf(Array);
+    expect(arrayReverse(['hello'])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
@@ -16,5 +17,26 @@ describe(`Function 'arrayReverse':`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it(`should correctly reverse a single-element array`, () => {
+    expect(arrayReverse(['Hello'])).toEqual(['olleH']);
+  });
+
+  it(`should correctly reverse and redistribute characters
+    for ['I', 'am', 'a', 'student!']`, () => {
+    expect(arrayReverse(['I', 'am', 'a', 'student!']))
+      .toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  });
+
+  it(`should return an empty array for an empty input array`, () => {
+    expect(arrayReverse([])).toEqual([]);
+  });
+
+  it(`should handle arrays containing empty strings`, () => {
+    expect(arrayReverse(['a', '', 'b'])).toEqual(['b', '', 'a']);
+    expect(arrayReverse(['', 'abc', ''])).toEqual(['', 'cba', '']);
+  });
+
+  it(`should handle strings with special characters and numbers`, () => {
+    expect(arrayReverse(['@1', 'test', '3$'])).toEqual(['$3', 'tset', '1@']);
+  });
 });
